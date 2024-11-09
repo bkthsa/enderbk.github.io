2f57f5803a773670518693531eb1205d0cbb3fc7
bkthsa <enderbk1279@gmail.com>
2024-11-09 20:26:47 +0700

    Update kmskeylist.bat

--- kmskeylist.bat
+++ kmskeylist.bat
@@ -280,5 +280,286 @@ echo Visio Standard 2010
 echo 767HD-QGMWX-8QTDB-9G3R2-KHFGJ
 echo Word 2010
 echo HVHB3-C6FV7-KQX9W-YQG79-CRY7T
+
+echo Press any key to exit...
+echo Microsoft Windows and Office KMS Keys
+echo Windows Server 2022 Datacenter
+echo WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+echo Windows Server 2022 Datacenter Azure Edition
+echo NTBV8-9K7Q8-V27C6-M2BTV-KHMXV
+echo Windows Server 2022 Standard
+echo VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+echo Windows Server 2022 Standard
+echo VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+echo Windows Server 2019 Datacenter
+echo WMDGN-G9PQG-XVVXX-R3X43-63DFG
+echo Windows Server 2019 Essentials
+echo WVDHN-86M7X-466P6-VHXV7-YY726
+echo Windows Server 2019 Standard
+echo N69G4-B89J2-4G8F4-WWYCC-J464C
+echo Windows Server Datacenter
+echo 6NMRW-2C8FM-D24W7-TQWMY-CWH2D
+echo Windows Server Standard
+echo N2KJX-J94YW-TQVFB-DG9YT-724CC
+echo Windows Server Datacenter
+echo 2HXDN-KRXHB-GPYC7-YCKFJ-7FVDG
+echo Windows Server Standard
+echo PTXN8-JFHJM-4WC78-MPCBR-9W4KR
+echo Windows Server Datacenter
+echo 6Y6KB-N82V8-D8CQV-23MJW-BWTG6
+echo Windows Server Standard
+echo DPCNP-XQFKJ-BJF7R-FRC8D-GF6G4
+echo Windows Server 2016 Datacenter
+echo CB7KF-BWN84-R7R2Y-793K2-8XDDG
+echo Windows Server 2016 Essentials
+echo JCKRF-N37P4-C2D82-9YXRT-4M63B
+echo Windows Server 2016 Standard
+echo WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY
+echo Windows Server 2012 R2 Datacenter
+echo W3GGN-FT8W3-Y4M27-J84CP-Q3VJ9
+echo Windows Server 2012 R2 Essentials
+echo KNC87-3J2TX-XB4WP-VCPJV-M4FWM
+echo Windows Server 2012 R2 Server Standard
+echo D2N9P-3P6X9-2R39C-7RTCD-MDVJX
+echo Windows Server 2012 Country Specific
+echo 4K36P-JN4VD-GDC6V-KDT89-DYFKP
+echo Windows Server 2012 Datacenter
+echo 48HP8-DN98B-MYWDG-T2DCC-8W83P
+echo Windows Server 2012 MultiPoint Premium
+echo XNH6W-2V9GX-RGJ4K-Y8X6F-QGJ2G
+echo Windows Server 2012 MultiPoint Standard
+echo HM7DN-YVMH3-46JC3-XYTG7-CYQJJ
+echo Windows Server 2012 N
+echo 8N2M2-HWPGY-7PGT9-HGDD8-GVGGY
+echo Windows Server 2012 Server Standard
+echo XC9B7-NBPP2-83J2H-RHMBY-92BT4
+echo Windows Server 2012 Single Language
+echo 2WN2H-YGCQR-KFX6K-CD6TF-84YXQ
+echo Windows Server 2012
+echo BN3D2-R7TKB-3YPBD-8DRP2-27GG4
+echo Windows Server 2008 R2 Web
+echo 6TPJF-RBVHG-WBW2R-86QPH-6RTM4
+echo Windows Server 2008 R2 HPC edition
+echo TT8MH-CG224-D3D7Q-498W2-9QCTX
+echo Windows Server 2008 R2 Standard
+echo YC6KT-GKW9T-YTKYR-T4X34-R7VHC
+echo Windows Server 2008 R2 Enterprise
+echo 489J6-VHDMP-X63PK-3K798-CPX3Y
+echo Windows Server 2008 R2 Datacenter
+echo 74YFP-3QFB3-KQT8W-PMXWJ-7M648
+echo Windows Server 2008 R2 for Itanium-based Systems
+echo GT63C-RJFQ3-4GMB6-BRFB9-CB83V
+echo Windows Server 2008 Datacenter
+echo 7M67G-PC374-GR742-YH8V4-TCBY3
+echo Windows Server 2008 Datacenter without Hyper-V
+echo 22XQ2-VRXRG-P8D42-K34TD-G3QQC
+echo Windows Server 2008 Enterprise
+echo YQGMW-MPWTJ-34KDK-48M3W-X4Q6V
+echo Windows Server 2008 Enterprise without Hyper-V
+echo 39BXF-X8Q23-P2WWT-38T2F-G3FPG
+echo Windows Server 2008 HPC
+echo RCTX3-KWVHP-BR6TB-RB6DM-6X7HP
+echo Windows Server 2008 Standard without Hyper-V
+echo W7VD6-7JFBR-RX26B-YKQ3Y-6FFFJ
+echo Windows Server 2008 Standard
+echo TM24T-X9RMF-VWXK6-X8JC9-BFGM2
+echo Windows Server 2008 for Itanium-Based Systems
+echo 4DWFP-JF3DJ-B7DTH-78FJB-PDRHK
+echo Windows Web Server 2008
+echo WYR28-R7TFJ-3X2YQ-YCY4H-M249D
+echo Windows 11 Education N
+echo 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+echo Windows 11 Education
+echo NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+echo Windows 11 Enterprise G N
+echo 44RPN-FTY23-9VTTB-MP9BX-T84FV
+echo Windows 11 Enterprise G
+echo YYVX9-NTFWV-6MDM3-9PT4T-4M68B
+echo Windows 11 Enterprise N
+echo DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+echo Windows 11 Enterprise
+echo NPPR9-FWDCX-D2C8J-H872K-2YT43
+echo Windows 11 Pro Education N
+echo YVWGF-BXNMC-HTQYQ-CPQ99-66QFC
+echo Windows 11 Pro Education
+echo 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+echo Windows 11 Pro N
+echo MH37W-N47XK-V7XM9-C7227-GCQG9
+echo Windows 11 Pro for Workstations N
+echo 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+echo Windows 11 Pro for Workstations
+echo NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+echo Windows 11 Pro
+echo W269N-WFGWX-YVC9B-4J6C9-T83GX
+echo Windows 10 Professional Workstation
+echo NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+echo Windows 10 Professional Workstation N
+echo 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+echo Windows 10 Education N
+echo 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+echo Windows 10 Education
+echo NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+echo Windows 10 Enterprise 2015 LTSB N
+echo 2F77B-TNFGY-69QQF-B8YKP-D69TJ
+echo Windows 10 Enterprise 2015 LTSB
+echo WNMTR-4C88C-JK8YV-HQ7T2-76DF9
+echo Windows 10 Enterprise 2016 LTSB N
+echo QFFDN-GRT3P-VKWWX-X7T3R-8B639
+echo Windows 10 Enterprise 2016 LTSB
+echo DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ
+echo Windows 10 Enterprise N
+echo DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+echo Windows 10 Enterprise
+echo NPPR9-FWDCX-D2C8J-H872K-2YT43
+echo Windows 10 Professional N
+echo MH37W-N47XK-V7XM9-C7227-GCQG9
+echo Windows 10 Professional
+echo W269N-WFGWX-YVC9B-4J6C9-T83GX
+echo Access LTSC 2021
+echo WM8YG-YNGDD-4JHDC-PG3F4-FC4T4
+echo Excel LTSC 2021
+echo NWG3X-87C9K-TC7YY-BC2G7-G6RVC
+echo Office LTSC Professional Plus 2021
+echo FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+echo Office LTSC Standard 2021
+echo KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3
+echo Outlook LTSC 2021
+echo C9FM6-3N72F-HFJXB-TM3V9-T86R9
+echo PowerPoint LTSC 2021
+echo TY7XF-NFRBR-KJ44C-G83KF-GX27K
+echo Project Professional 2021
+echo FTNWT-C6WBT-8HMGF-K9PRX-QV9H8
+echo Project Standard 2021
+echo J2JDC-NJCYY-9RGQ4-YXWMH-T3D4T
+echo Publisher LTSC 2021
+echo 2MW9D-N4BXM-9VBPG-Q7W6M-KFBGQ
+echo Skype for Business LTSC 2021
+echo HWCXN-K3WBT-WJBKY-R8BD9-XK29P
+echo Visio LTSC Professional 2021
+echo KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4
+echo Visio LTSC Standard 2021
+echo MJVNY-BYWPY-CWV6J-2RKRT-4M8QG
+echo Word LTSC 2021
+echo TN8H9-M34D3-Y64V9-TR72V-X79KV
+echo Access 2019
+echo 9N9PT-27V4Y-VJ2PD-YXFMF-YTFQT
+echo Excel 2019
+echo TMJWT-YYNMB-3BKTF-644FC-RVXBD
+echo Office Professional Plus 2019
+echo NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+echo Office Standard 2019
+echo 6NWWJ-YQWMR-QKGCB-6TMB3-9D9HK
+echo Outlook 2019
+echo 7HD7K-N4PVK-BHBCQ-YWQRW-XW4VK
+echo PowerPoint 2019
+echo RRNCX-C64HY-W2MM7-MCH9G-TJHMQ
+echo Project Professional 2019
+echo B4NPR-3FKK7-T2MBV-FRQ4W-PKD2B
+echo Project Standard 2019
+echo C4F7P-NCP8C-6CQPT-MQHV9-JXD2M
+echo Publisher 2019
+echo G2KWX-3NW6P-PY93R-JXK2T-C9Y9V
+echo Skype for Business 2019
+echo NCJ33-JHBBY-HTK98-MYCV8-HMKHJ
+echo Visio Professional 2019
+echo 9BGNQ-K37YR-RQHF2-38RQ3-7VCBB
+echo Visio Standard 2019
+echo 7TQNQ-K3YQQ-3PFH7-CCPPM-X4VQ2
+echo Word 2019
+echo PBX3G-NWMT6-Q7XBW-PYJGG-WXD33
+echo Access 2016
+echo GNH9Y-D2J4T-FJHGG-QRVH7-QPFDW
+echo Excel 2016
+echo 9C2PK-NWTVB-JMPW8-BFT28-7FTBF
+echo Office Professional Plus 2016
+echo XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+echo Office Standard 2016
+echo JNRGM-WHDWX-FJJG3-K47QV-DRTFM
+echo OneNote 2016
+echo DR92N-9HTF2-97XKM-XW2WJ-XW3J6
+echo Outlook 2016
+echo R69KK-NTPKF-7M3Q4-QYBHW-6MT9B
+echo PowerPoint 2016
+echo J7MQP-HNJ4Y-WJ7YM-PFYGF-BY6C6
+echo Project Professional 2016
+echo YG9NW-3K39V-2T3HJ-93F3Q-G83KT
+echo Project Standard 2016
+echo GNFHQ-F6YQM-KQDGJ-327XX-KQBVC
+echo Publisher 2016
+echo F47MM-N3XJP-TQXJ9-BP99D-8837K
+echo Publisher 2016
+echo F47MM-N3XJP-TQXJ9-BP99D-8K837
+echo Skype for Business 2016
+echo 869NQ-FJ69K-466HW-QYCP2-DDBV6
+echo Visio Professional 2016
+echo PD3PC-RHNGV-FXJ29-8JK7D-RJRJK
+echo Visio Standard 2016
+echo 7WHWN-4T7MP-G96JF-G33KR-W8GF4
+echo Word 2016
+echo WXY84-JN2Q9-RBCCQ-3Q3J3-3PFJ6
+echo Access 2013
+echo NG2JY-H4JBT-HQXYP-78QH9-4JM2D
+echo Excel 2013
+echo VGPNG-Y7HQW-9RHP7-TKPV3-BG7GB
+echo InfoPath 2013
+echo DKT8B-N7VXH-D963P-Q4PHY-F8894
+echo Lync 2013
+echo 2MG3G-3BNTT-3MFW9-KDQW3-TCK7R
+echo Office 2013 Professional Plus
+echo YC7DK-G2NP3-2QQC3-J6H88-GVGXT
+echo Office 2013 Standard
+echo KBKQT-2NMXY-JJWGP-M62JB-92CD4
+echo OneNote 2013
+echo TGN6P-8MMBC-37P2F-XHXXK-P34VW
+echo Outlook 2013
+echo QPN8Q-BJBTJ-334K3-93TGY-2PMBT
+echo PowerPoint 2013
+echo 4NT99-8RJFH-Q2VDH-KYG2C-4RD4F
+echo Project 2013 Professional
+echo FN8TT-7WMH6-2D4X9-M337T-2342K
+echo Project 2013 Standard
+echo 6NTH3-CW976-3G3Y2-JK3TX-8QHTT
+echo Publisher 2013
+echo PN2WF-29XG2-T9HJ7-JQPJR-FCXK4
+echo Visio 2013 Professional
+echo C2FG9-N6J68-H8BTJ-BW3QX-RM3B3
+echo Visio 2013 Standard
+echo J484Y-4NKBF-W2HMG-DBMJC-PGWR7
+echo Word 2013
+echo 6Q7VD-NX8JD-WJ2VH-88V73-4GBJ7
+echo Access 2010
+echo V7Y44-9T38C-R2VJK-666HK-T7DDX
+echo Excel 2010
+echo H62QG-HXVKF-PP4HP-66KMR-CW9BM
+echo InfoPath 2010
+echo K96W8-67RPQ-62T9Y-J8FQJ-BT37T
+echo Office Home and Business 2010
+echo D6QFG-VBYP2-XQHM7-J97RH-VVRCK
+echo Office Professional Plus 2010
+echo VYBBJ-TRJPB-QFQRF-QFT4D-H3GVB
+echo Office Standard 2010
+echo V7QKV-4XVVR-XYV4D-F7DFM-8R6BM
+echo OneNote 2010
+echo Q4Y4M-RHWJM-PY37F-MTKWH-D3XHX
+echo Outlook 2010
+echo 7YDC2-CWM8M-RRTJC-8MDVC-X3DWQ
+echo PowerPoint 2010
+echo RC8FX-88JRY-3PF7C-X8P67-P4VTT
+echo Project Professional 2010
+echo YGX6F-PGV49-PGW3J-9BTGG-VHKC6
+echo Project Standard 2010
+echo 4HP3K-88W3F-W2K3D-6677X-F9PGB
+echo Publisher 2010
+echo BFK7F-9MYHM-V68C7-DRQ66-83YTP
+echo SharePoint Workspace 2010
+echo QYYW6-QP4CB-MBV6G-HYMCJ-4T3J4
+echo Visio Premium 2010
+echo D9DWC-HPYVV-JGF4P-BTWQB-WX8BJ
+echo Visio Professional 2010
+echo 7MCW8-VRQVK-G677T-PDJCM-Q8TCP
+echo Visio Standard 2010
+echo 767HD-QGMWX-8QTDB-9G3R2-KHFGJ
+echo Word 2010
+echo HVHB3-C6FV7-KQX9W-YQG79-CRY7T
 echo Press any key to exit...
 pause > NUL
